9386a0e195f1185c583e3079039a67d282fcd10d
Leo Kalshteyn <leo.kalshteyn@gmail.com>
2019-11-30 02:19:11 +0000

    Automatically backed up by Learn

--- src/reducers/manageAuthorsAndBooks.js
+++ src/reducers/manageAuthorsAndBooks.js
@@ -27,11 +27,11 @@ function authorsReducer(state = [], action) {
   let idx;
   switch (action.type) {
     case "ADD_AUTHOR":
-    return [...state, action.author];
+      return [...state, action.author];
 
     case "REMOVE_AUTHOR":
-    idx = state.findIndex(author => author.id === action.id);
-    return [...state.slice(0, idx), ...state.slice(idx + 1)]
+      idx = state.findIndex(author => author.id === action.id);
+      return [...state.slice(0, idx), ...state.slice(idx + 1)]
 
     case "ADD_BOOK":
       let existingAuthor = state.filter(
