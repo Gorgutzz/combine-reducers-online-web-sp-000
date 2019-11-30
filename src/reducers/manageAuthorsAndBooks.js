de9c89fa98ea7671772325ba5ebe789c4769775e
Leo Kalshteyn <leo.kalshteyn@gmail.com>
2019-11-30 02:15:44 +0000

    Automatically backed up by Learn

--- src/reducers/manageAuthorsAndBooks.js
+++ src/reducers/manageAuthorsAndBooks.js
@@ -12,7 +12,7 @@ function booksReducer(state = [], action) {
   let idx;
   switch (action.type) {
     case "ADD_BOOK":
-return [...state, action.book];
+      return [...state, action.book];
 
     case "REMOVE_BOOK":
       idx = state.books.findIndex(book => book.id === action.id);
