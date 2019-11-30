cc945890308019bbdf68afca6b48fb3a84ae38d4
Leo Kalshteyn <leo.kalshteyn@gmail.com>
2019-11-30 02:14:54 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ import rootReducer from './reducers/manageAuthorsAndBooks';
 
 
 const store = createStore(rootReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
+
 ReactDOM.render(
   <Provider store={store}>
     <App store={store}/>
